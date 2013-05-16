dea0fab336662ee52099f7335a2421fe8c758732
Ashley Ruen <ruenashl@msu.edu>
2013-05-15 21:52:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RuenashlBlog::Application.config.secret_token = '903353aea2e1fe9629f5154cedbe78cba880abfcdd10967327feb4a6ab45a4de049123ee050527bdba5d34b64fd88e026b1746cb6d52ffaa4eb969e6e7ddd6bf'
