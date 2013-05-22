29208f0fc5c7d216011a3b165dea32e6851e0214
Ashley Ruen <ruenashl@msu.edu>
2013-05-21 23:22:56 -0400

    Setup post, comment and status models, create scaffolding for main blod data items

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  def create
+    # We need to get the post so that Rails knows where to assign the comment
+    @post = Post.find(params[:post_id])
+    
+    # Here we create a new comment based on the POST data from our form (params[:comment])
+    @comment = @post.comments.create(params[:comment])
+    
+    # Once the post is saved, we redirect the user back to the post they were viewing
+    redirect_to post_path(@post)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 RuenashlBlog::Application.routes.draw do
+  resources :statuses
+
+
+  resources :posts do
+    resources :comments
+  end
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
