2b279c6807b298f8626a3f06700902cb01d2f54c
Ashley Ruen <ruenashl@msu.edu>
2013-06-05 12:08:00 -0400

    Week 4 TEsting/Validating

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   attr_accessible :author_email, :author_name, :author_url, :body
+  
+  # Validations
+  validates :author_name, :author_email, :body, :presence => true
+  validates :author_email, :format => { :with => /@/, :message => "Your email address is not valid" }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :status
   attr_accessible :body, :status_id, :title
+  
+  # Validations
+  validates :title, :body, :status_id, :presence => true
+  validates :title, :uniqueness => true
 end
