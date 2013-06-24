659b3f767bda8938b6725ebd79b42719e4a2116a
Ashley Ruen <ruenashl@msu.edu>
2013-06-24 19:02:45 -0400

    s

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,12 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Status.find_by_name("Published").posts
-    @drafts = Status.find_by_name("Draft").posts
+    @posts = Status.find_by_name("Published").post
+    @drafts = Status.find_by_name("Draft").post
     
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @posts }
+      format.json { render json: @post }
     end
   end
 
