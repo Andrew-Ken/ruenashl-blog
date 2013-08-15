3ec680ec654687d444aa60db2789b2a30c10edee
Ashley Ruen <ruenashl@msu.edu>
2013-08-15 09:22:32 -0400

    compile js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ RuenashlBlog::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( libs/jquery.min.js libs/jquery.imagesloaded.js libs/jquery.colorbox-min.js libs/jquery.wookmark.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
